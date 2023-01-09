6eaf2ac2f3129764cf945999e52f9357736fcb0e
Kristiyan Michaylov <krisidm24@gmail.com>
2023-01-09 21:02:35 +0100

    Show simple Setup structure for the daily challenges

--- src/common/Setup.kt
+++ src/common/Setup.kt
@@ -1,2 +1,17 @@
 package common
 
+import java.io.File
+
+abstract class Day(val day: Int, val file: String) {
+
+    var lines = mutableListOf<String>()
+
+    protected fun readInput(fileName: String) {
+        File(fileName).forEachLine { lines.add(it) }
+    }
+
+    protected abstract fun part1()
+
+    protected abstract fun part2()
+
+}
